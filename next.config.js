8aeadec51d3e7ad1ededf47362e88f63c5a79f10
Patea Tlau <pateatlau@gmail.com>
2025-03-11 16:48:15 +0530

    Disabled static site deploy in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  // output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
     ],
   },
