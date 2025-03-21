09d7c0412f93bef30a955f7c7da2e1682a8fdc94
Patea Tlau <pateatlau@gmail.com>
2025-03-21 08:43:48 +0530

    Sentry monitoring tool implemented

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -9,7 +9,7 @@ export default function ThemeSwitch() {
 
   return (
     <button
-      className="fixed bottom-5 right-5 bg-yellow-50 w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-yellow-100 border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 dark:border-gray-800"
+      className="fixed bottom-20 right-5 bg-yellow-50 w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-yellow-100 border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 dark:border-gray-800"
       onClick={toggleTheme}
       title={theme === 'light' ? 'Switch to dark mode' : 'Switch to light mode'}
     >
