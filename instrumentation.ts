3e61e004e4bf4e163144f075908e3f20af4c6338
Patea Tlau <pateatlau@gmail.com>
2025-04-27 13:33:08 +0530

    instrumentation.js updated

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,9 @@
 import * as Sentry from '@sentry/nextjs';
 
+// Sentry token
+// lalding-token
+// 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
+
 export async function register() {
   // if (process.env.NEXT_RUNTIME === 'nodejs') {
   //   await import('./sentry.server.config');
